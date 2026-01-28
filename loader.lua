1e8f1ca28c27197e9aae6db8290aac778264d1ac
DuxiiT <johony.mahfoud06@gmail.com>
2026-01-28 18:19:42 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -15,6 +15,14 @@ local Games = {
 
     [17625359962] = function()
         loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a3e94a0738157c3bafa3b66b389dddab95542bf40f6cc17f7bd4cbab5ca37d78/download"))()
+    end,
+
+    [136801880565837] = function()
+        loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a3e94a0738157c3bafa3b66b389dddab95542bf40f6cc17f7bd4cbab5ca37d78/download"))()
+    end,
+
+    [301549746] = function()
+        loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a3e94a0738157c3bafa3b66b389dddab95542bf40f6cc17f7bd4cbab5ca37d78/download"))()
     end
 }
 
